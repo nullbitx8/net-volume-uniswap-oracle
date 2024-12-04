8b6c86a15559ec3a7253cd7160b518e9a599db29
nullbitx8 <nullbitx8@proton.me>
2024-12-04 09:37:13 +0000

    Add tests and funcitonality for Volume Oracle to observe TWANV (time weighted average net volume) over a specified start time and end time; TODO fix failing test

--- src/NetVolumeOracleV2.sol
+++ src/NetVolumeOracleV2.sol
@@ -44,7 +44,7 @@ contract NetVolumeOracleV2 is BaseHook {
         return
             Hooks.Permissions({
                 beforeInitialize: false,
-                afterInitialize: false,
+                afterInitialize: true,
                 beforeAddLiquidity: false,
                 beforeRemoveLiquidity: false,
                 afterAddLiquidity: false,
@@ -58,6 +58,18 @@ contract NetVolumeOracleV2 is BaseHook {
                 afterAddLiquidityReturnDelta: false,
                 afterRemoveLiquidityReturnDelta: false
             });
+    }
+
+    
+    function afterInitialize(address, PoolKey calldata key, uint160, int24)
+        external
+        override
+        onlyPoolManager
+        returns (bytes4)
+    {
+        PoolId id = key.toId();
+        (states[id].cardinality, states[id].cardinalityNext) = observations[id].initialize(uint32(block.timestamp));
+        return this.afterInitialize.selector;
     }
 
     function afterSwap(
@@ -78,14 +90,85 @@ contract NetVolumeOracleV2 is BaseHook {
             states[id].cardinalityNext
         );
 
-        /*
-        // extract user from hookData and update their net volume
-        address user = abi.decode(hookData, (address));
-        userNetVolumeToken0[poolId][user] += delta.amount0();
-        userNetVolumeToken1[poolId][user] += delta.amount1();
-        */
-
         return (this.afterSwap.selector, 0);
     }
-}
 
+    /// @notice Observe the given pool for the timestamps
+    function observe(PoolKey calldata key, uint32[] calldata secondsAgos)
+        external
+        view
+        returns (int256[] memory token0VolumeCumulatives, int256[] memory token1VolumeCumulatives)
+    {
+        PoolId id = key.toId();
+
+        ObservationState memory state = states[id];
+
+        return observations[id].observe(
+            uint32(block.timestamp),
+            secondsAgos,
+            state.index,
+            state.cardinality
+        );
+    }
+
+    /// @notice Returns the state for the given pool key
+    function getState(PoolKey calldata key) 
+        external
+        view
+        returns (ObservationState memory) {
+        return states[key.toId()];
+    }
+
+    /// @notice Returns the observation for the given pool key and observation index
+    function getObservation(PoolKey calldata key, uint256 index)
+        external
+        view
+        returns (Oracle.Observation memory)
+    {
+        return observations[key.toId()][index];
+    }
+
+    /// @notice Increase the cardinality target for the given pool
+    function increaseCardinalityNext(PoolKey calldata key, uint16 cardinalityNext)
+        external
+        returns (uint16 cardinalityNextOld, uint16 cardinalityNextNew)
+    {
+        PoolId id = key.toId();
+
+        ObservationState storage state = states[id];
+
+        cardinalityNextOld = state.cardinalityNext;
+        cardinalityNextNew = observations[id].grow(cardinalityNextOld, cardinalityNext);
+        state.cardinalityNext = cardinalityNextNew;
+    }
+
+    function getNetVolume(PoolKey calldata key, uint32 startTime, uint32 endTime)
+        external
+        view
+        returns (int256 token0NetVolume, int256 token1NetVolume)
+    {
+        PoolId id = key.toId();
+
+        ObservationState memory state = states[id];
+
+        uint32[] memory secondsAgos = new uint32[](2);
+        secondsAgos[0] = uint32(block.timestamp) - startTime;
+        secondsAgos[1] = uint32(block.timestamp) - endTime;
+
+        (
+            int256[] memory token0VolumeCumulatives,
+            int256[] memory token1VolumeCumulatives
+        ) = observations[id].observe(
+            uint32(block.timestamp),
+            secondsAgos,
+            state.index,
+            state.cardinality
+        );
+
+        token0NetVolume = (token0VolumeCumulatives[1] - token0VolumeCumulatives[0])
+            / int256(uint256(endTime - startTime));
+
+        token1NetVolume = (token1VolumeCumulatives[1] - token1VolumeCumulatives[0])
+            / int256(uint256(endTime - startTime));
+    }
+}
--- test/NetVolumeOracleV2.t.sol
+++ test/NetVolumeOracleV2.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
-import "forge-std/console.sol";
+import {console} from "forge-std/console.sol";
 
 import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
 import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
@@ -17,7 +17,8 @@ import {PoolId} from "v4-core/types/PoolId.sol";
 import {IHooks} from "v4-core/interfaces/IHooks.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 
-import {NetVolumeOracleV1} from "../src/NetVolumeOracleV1.sol";
+import {Oracle} from "../src/libraries/Oracle.sol";
+import {NetVolumeOracleV2} from "../src/NetVolumeOracleV2.sol";
 
 contract TestNetVolumeOracleV2 is Test, Deployers {
     using CurrencyLibrary for Currency;
@@ -26,15 +27,15 @@ contract TestNetVolumeOracleV2 is Test, Deployers {
     Currency tokenCurrency;
     PoolId poolId;
 
-    NetVolumeOracleV1 hook;
+    NetVolumeOracleV2 hook;
 
     function setUp() public {
         deployFreshManagerAndRouters();
         deployMintAndApprove2Currencies();
 
         // deploy hook to an address that has the proper flags set
-        uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG);
-        hook = NetVolumeOracleV1(address(flags));
+        uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG | Hooks.AFTER_INITIALIZE_FLAG);
+        hook = NetVolumeOracleV2(address(flags));
         deployCodeTo("NetVolumeOracleV2.sol", abi.encode(manager), address(hook));
 
         (key,) = initPoolAndAddLiquidity(
@@ -44,6 +45,120 @@ contract TestNetVolumeOracleV2 is Test, Deployers {
 
         vm.label(Currency.unwrap(currency0), "currency0");
         vm.label(Currency.unwrap(currency1), "currency1");
+    }
+
+    function testCanAttachToPoolsWithDifferentFees() public {
+        initPoolAndAddLiquidity(
+            currency0, currency1, IHooks(address(hook)), 300, SQRT_PRICE_1_1
+        );
+
+        initPoolAndAddLiquidity(
+            currency0, currency1, IHooks(address(hook)), 500, SQRT_PRICE_1_1
+        );
+
+        initPoolAndAddLiquidity(
+            currency0, currency1, IHooks(address(hook)), 1000, SQRT_PRICE_1_1
+        );
+    }
+
+    function testAfterInitializeState() public {
+        NetVolumeOracleV2.ObservationState memory observationState = hook.getState(key);
+        assertEq(observationState.index, 0);
+        assertEq(observationState.cardinality, 1);
+        assertEq(observationState.cardinalityNext, 1);
+    }
+
+    function testAfterInitializeObservation() public {
+        Oracle.Observation memory observation = hook.getObservation(key, 0);
+        assertTrue(observation.initialized);
+        assertEq(observation.blockTimestamp, 1);
+        assertEq(observation.token0VolumeCumulative, 0);
+        assertEq(observation.token1VolumeCumulative, 0);
+    }
+
+    function testAfterInitializeObserve0() public {
+        uint32[] memory secondsAgo = new uint32[](1);
+        secondsAgo[0] = 0;
+        (int256[] memory token0VolumeCumulatives, int256[] memory token1VolumeCumulatives) = hook.observe(key, secondsAgo);
+        assertEq(token0VolumeCumulatives.length, 1);
+        assertEq(token1VolumeCumulatives.length, 1);
+        assertEq(token0VolumeCumulatives[0], 0);
+        assertEq(token1VolumeCumulatives[0], 0);
+    }
+
+    function testObserveAfterSwapZeroForOne() public {
+        BalanceDelta delta = swap_zeroForOne(100);
+
+        uint32[] memory secondsAgo = new uint32[](1);
+        secondsAgo[0] = 0;
+        (int256[] memory token0VolumeCumulatives, int256[] memory token1VolumeCumulatives) = hook.observe(key, secondsAgo);
+
+        assertEq(token0VolumeCumulatives.length, 1);
+        assertEq(token1VolumeCumulatives.length, 1);
+        assertEq(token0VolumeCumulatives[0], delta.amount0());
+        assertEq(token1VolumeCumulatives[0], delta.amount1());
+    }
+
+    function testObserveAfterSwapOneForZer0() public {
+        BalanceDelta delta = swap_oneForZero(100);
+
+        uint32[] memory secondsAgo = new uint32[](1);
+        secondsAgo[0] = 0;
+        (int256[] memory token0VolumeCumulatives, int256[] memory token1VolumeCumulatives) = hook.observe(key, secondsAgo);
+
+        assertEq(token0VolumeCumulatives.length, 1);
+        assertEq(token1VolumeCumulatives.length, 1);
+        assertEq(token0VolumeCumulatives[0], delta.amount0());
+        assertEq(token1VolumeCumulatives[0], delta.amount1());
+    }
+
+    function testObserveAfterSwapZeroForOne20SecondsLater() public {
+        BalanceDelta delta = swap_zeroForOne(100);
+        vm.warp(block.timestamp + 20);
+
+        uint32[] memory secondsAgo = new uint32[](1);
+        secondsAgo[0] = 0;
+        (int256[] memory token0VolumeCumulatives, int256[] memory token1VolumeCumulatives) = hook.observe(key, secondsAgo);
+
+        assertEq(token0VolumeCumulatives.length, 1);
+        assertEq(token1VolumeCumulatives.length, 1);
+        assertEq(token0VolumeCumulatives[0], delta.amount0() + delta.amount0() * 20);
+        assertEq(token1VolumeCumulatives[0], delta.amount1() + delta.amount1() * 20);
+    }
+
+    function testObserve20SecondsAgoAfterSwapAfter20Seconds() public {
+        BalanceDelta delta = swap_zeroForOne(100);
+        vm.warp(block.timestamp + 20);
+
+        uint32[] memory secondsAgo = new uint32[](1);
+        secondsAgo[0] = 20;
+        (int256[] memory token0VolumeCumulatives, int256[] memory token1VolumeCumulatives) = hook.observe(key, secondsAgo);
+
+        assertEq(token0VolumeCumulatives.length, 1);
+        assertEq(token1VolumeCumulatives.length, 1);
+        assertEq(token0VolumeCumulatives[0], delta.amount0());
+        assertEq(token1VolumeCumulatives[0], delta.amount1());
+    }
+
+    function testIncreaseCardinalityNext() public {
+        // increase cardinality next
+        hook.increaseCardinalityNext(key, 3);
+
+        // assert state
+        NetVolumeOracleV2.ObservationState memory observationState = hook.getState(key);
+        assertEq(observationState.index, 0);
+        assertEq(observationState.cardinality, 1);
+        assertEq(observationState.cardinalityNext, 3);
+
+        // conduct swap
+        vm.warp(block.timestamp + 12);  // advance a block
+        swap_zeroForOne(100);
+
+        // assert new state
+        observationState = hook.getState(key);
+        assertEq(observationState.index, 1);
+        assertEq(observationState.cardinality, 3);
+        assertEq(observationState.cardinalityNext, 3);
     }
 
     /* Util function for swapping token0 to token1 */
@@ -99,4 +214,36 @@ contract TestNetVolumeOracleV2 is Test, Deployers {
 
         return delta;
     }
+
+    // get time weighted average net volume for a pool during a given time period
+    function testGetNetVolume() public {
+        hook.increaseCardinalityNext(key, 3);
+
+        // make first swap
+        BalanceDelta deltaFirst = swap_zeroForOne(100);
+
+        // set the times to be observed
+        uint32 startTime = uint32(block.timestamp) + 20;
+        uint32 endTime = uint32(block.timestamp) + 40;
+
+        // make second swap
+        vm.warp(startTime);
+        BalanceDelta deltaSecond = swap_zeroForOne(200);
+
+        // make third swap
+        vm.warp(block.timestamp + 20);
+        BalanceDelta deltaThird = swap_zeroForOne(300);
+
+        // get net volume
+        (int256 token0NetVolume, int256 token1NetVolume) = hook.getNetVolume(
+            key, 
+            startTime,
+            endTime
+        );
+        
+        // assert net volume
+        assertEq(token0NetVolume, (deltaSecond.amount0() + deltaThird.amount0()) / 2);
+        assertEq(token1NetVolume, (deltaSecond.amount1() + deltaThird.amount1()) / 2);
+    }
+
 }
